b7695b1db7b15bbfa1e0d50167646838d4f0ce05
AMJAD <amjad@MacBook-Air.lan>
2023-04-24 16:17:25 +0200

    fix colors

--- src/components/StyledComponent.js
+++ src/components/StyledComponent.js
@@ -4,7 +4,11 @@ import styled from "styled-components";
 export const StyledContainer = styled.div`
   display: grid;
   place-items: center;
-  margin: 3rem;
+  margin: 10rem auto;
+
+  @media (max-width: 768px) {
+    margin: 5rem auto;
+  }
 `;
 
 export const StyledHeader = styled.div`
@@ -12,18 +16,29 @@ export const StyledHeader = styled.div`
   justify-content: space-between;
   margin-bottom: 3rem;
   width: 100%;
+  gap: 3rem;
+  @media (max-width: 768px) {
+    gap: 1.5rem;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export const StyledSlider = styled(Slider)`
   width: 400px;
-  margin-top: 1rem;
-  margin-bottom: 4rem;
+  margin-top: 0.5rem;
+  margin-bottom: 5rem;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 
   .rc-slider-rail {
     height: 30px;
     border: 1px solid #ccc;
     background-color: transparent;
     border-radius: 5rem;
+
     &::after {
       white-space: pre;
       position: relative;
@@ -35,6 +50,9 @@ export const StyledSlider = styled(Slider)`
           return `${props.max.toLocaleString("sv-SE")} kr`;
         else return `${props.max.toLocaleString("sv-SE")} år`;
       }}";
+      @media (max-width: 768px) {
+        right: ${(props) => (props.name === "amount" ? "-68%" : "-82%")};
+      }
     }
   }
   .rc-slider-track {
@@ -65,14 +83,36 @@ export const StyledSlider = styled(Slider)`
   .rc-slider-handle::before {
     white-space: pre;
     position: relative;
-    top: 15px;
-    color: #fff;
-    left: ${(props) => (props.name === "amount" ? "-90px" : "-50px")};
+    top: ${(props) =>
+      (props.name.toLowerCase() === "amount" && props.value <= "100000") ||
+      props.value <= "3"
+        ? "3rem"
+        : "15px"};
+    color: ${(props) =>
+      (props.name.toLowerCase() === "amount" && props.value <= "100000") ||
+      props.value <= "3"
+        ? "#000"
+        : "#fff"};
+    left: ${(props) =>
+      props.name.toLowerCase() === "amount"
+        ? props.value <= "100000"
+          ? "10px"
+          : "-85px"
+        : props.value <= "3"
+        ? "10px"
+        : "-45px"};
+
     content: "${(props) => {
       if (props.name.toLowerCase() === "amount")
         return `${props.value.toLocaleString("sv-SE")} kr`;
       else return `${props.value.toLocaleString("sv-SE")} år`;
     }}";
+    // @media (max-width: 768px) {
+    //   top: 3rem;
+    //   bottom: 0;
+    //   left: 0;
+    //   color: #000;
+    // }
   }
 `;
 
@@ -87,7 +127,7 @@ export const StyledButton = styled.button`
   font-size: 15px;
   float: right;
   cursor: pointer;
-`;
+}`;
 
 export const StyledNotification = styled.div`
   display: inline-block;
